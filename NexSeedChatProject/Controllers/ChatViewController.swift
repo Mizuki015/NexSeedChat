64cf9a821c2e71ed1c1c49a0d5774c27600c0753
城間海月 <mizushiro426426@gmail.com>
2019-12-11 13:50:01 +0800

    ControllerとObjectsをそれぞれ追加

---
+++ NexSeedChatProject/Controllers/ChatViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatViewController.swift
+//  NexSeedChatProject
+//
+//  Created by 城間海月 on 2019/12/11.
+//  Copyright © 2019 Mizuki. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ NexSeedChatProject/Objects/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  NexSeedChatProject
+//
+//  Created by 城間海月 on 2019/12/11.
+//  Copyright © 2019 Mizuki. All rights reserved.
+//
+
+import Foundation
